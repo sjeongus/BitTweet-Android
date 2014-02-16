e4394cda908489c5091769f49085d8227c5eb5e9
fuyutsukikaru <hirokinoryu@gmail.com>
2014-02-15 19:43:38 -0800

    Updated twitter4j library.

---
+++ owaranai/src/main/java/com/golden/owaranai/twitter/SecretKeys.java
@@ -0,0 +1,14 @@
+package com.golden.owaranai.twitter;
+
+public class SecretKeys {
+    private static String CONSUMER_KEY = "lhoj0RHeaS5t4jLZsLYkcw";
+    private static String CONSUMER_SECRET = "dD9UXBAzp1SIWpBr5wgqwVCtCjNFAUlm2uguqjiKLd8";
+
+    public static String getCONSUMER_KEY() {
+        return CONSUMER_KEY;
+    }
+
+    public static String getCONSUMER_SECRET() {
+        return CONSUMER_SECRET;
+    }
+}
