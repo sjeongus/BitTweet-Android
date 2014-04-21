39408351e9dee7227518c9658ed575dc579fc479
Eugen Rochko <eugen@zeonfederated.com>
2014-04-21 15:49:49 +0200

    Fixed merge mistake with the SecretKeys class usage, cleaned up HomeTimelineContent class at the same time.
    (The point of that change was that you don't need
    getters for a public static final String)

--- owaranai/src/main/java/com/golden/owaranai/twitter/HomeTimelineContent.java
+++ owaranai/src/main/java/com/golden/owaranai/twitter/HomeTimelineContent.java
@@ -1,31 +1,25 @@
 package com.golden.owaranai.twitter;
 
 import android.content.SharedPreferences;
+import android.util.Log;
+import twitter4j.*;
+import twitter4j.conf.ConfigurationBuilder;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import twitter4j.Status;
-import twitter4j.Twitter;
-import twitter4j.TwitterException;
-import twitter4j.TwitterFactory;
-import twitter4j.User;
-import twitter4j.conf.ConfigurationBuilder;
-
 // Retrieve the home timeline of the user
 public class HomeTimelineContent implements TimelineContent {
-
     public List<StatusItem> statuses = new ArrayList<StatusItem>();
     public Map<String, StatusItem> status_map = new HashMap<String, StatusItem>();
-
-    static final String PREF_KEY_OAUTH_TOKEN = "oauth_token";
-    static final String PREF_KEY_OAUTH_SECRET = "oauth_token_secret";
 
     private SharedPreferences mSharedPreferences;
     private Twitter twitter;
     private User user;
+
+    private static final String TAG = "HomeTimelineContent";
 
     public HomeTimelineContent(SharedPreferences preferences) {
         mSharedPreferences = preferences;
@@ -40,23 +34,27 @@ public class HomeTimelineContent implements TimelineContent {
     // Function that retrieves the user's timeline
     public void getTimeline() {
         try {
-            String oauthkey = mSharedPreferences.getString(PREF_KEY_OAUTH_TOKEN, "No user");
-            String oauthsecret = mSharedPreferences.getString(PREF_KEY_OAUTH_SECRET, "No secret");
-            ConfigurationBuilder confbuild = new ConfigurationBuilder();
-            confbuild.setOAuthAccessToken(oauthkey).setOAuthAccessTokenSecret(oauthsecret)
-                    .setOAuthConsumerKey(SecretKeys.getCONSUMER_KEY()).setOAuthConsumerSecret(SecretKeys.getCONSUMER_SECRET());
-            Twitter twitter = new TwitterFactory(confbuild.build()).getInstance();
-            User user = twitter.verifyCredentials();
+            String oauthKey = mSharedPreferences.getString(TwitterLoginActivity.PREF_KEY_OAUTH_TOKEN, "No user");
+            String oauthSecret = mSharedPreferences.getString(TwitterLoginActivity.PREF_KEY_OAUTH_SECRET, "No secret");
+            ConfigurationBuilder confBuild = new ConfigurationBuilder();
+
+            confBuild.setOAuthAccessToken(oauthKey)
+                    .setOAuthAccessTokenSecret(oauthSecret)
+                    .setOAuthConsumerKey(SecretKeys.CONSUMER_KEY)
+                    .setOAuthConsumerSecret(SecretKeys.CONSUMER_SECRET);
+
+            twitter = new TwitterFactory(confBuild.build()).getInstance();
+            user = twitter.verifyCredentials();
+
             List<Status> temp = twitter.getHomeTimeline();
-            System.out.println("Got up to here!");
+            Log.e(TAG, "Got up to here!");
+
             for (int i = 0; i < temp.size(); i++) {
-                //System.out.println(temp.get(i).getText());
                 addItem(new StatusItem(temp.get(i)));
             }
-        } catch (TwitterException te) {
-            te.printStackTrace();
-            System.out.println("Failed to get timeline: " + te.getMessage());
-            System.exit(-1);
+        } catch (TwitterException e) {
+            e.printStackTrace();
+            Log.e(TAG, "Failed to get timeline");
         }
     }
 }
