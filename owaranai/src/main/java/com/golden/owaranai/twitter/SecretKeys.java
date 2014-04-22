e3f88faa067c8cf6f583b73dc9c33ea8510bc624
southrop <southrop113@gmail.com>
2014-04-22 20:04:54 +0100

    Re-adding secretkeys file

---
+++ owaranai/src/main/java/com/golden/owaranai/twitter/SecretKeys.java
@@ -0,0 +1,6 @@
+package com.golden.owaranai.twitter;
+
+public class SecretKeys {
+    public static String CONSUMER_KEY = "FYoFTG1AMDh9SG1bLowPAQ";
+    public static String CONSUMER_SECRET = "3Fj2ZtZeQLdka2BZ3wBNiP0w40IOrMp71gRWUj5ftlU";
+}
