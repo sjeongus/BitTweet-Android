561110dc47ad06b78704b779179e072455da7ad9
fuyutsukikaru <hirokinoryu@gmail.com>
2014-06-16 14:31:00 -0700

    Started implementation of an image uploader.

--- bittweet/src/main/java/org/bittweet/android/ui/NewTweetActivity.java
+++ bittweet/src/main/java/org/bittweet/android/ui/NewTweetActivity.java
@@ -2,13 +2,21 @@ package org.bittweet.android.ui;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.NavUtils;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.*;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import org.bittweet.android.ApplicationController;
 import org.bittweet.android.R;
@@ -17,6 +25,10 @@ import org.bittweet.android.services.TweetService;
 import org.bittweet.android.ui.adapters.SimpleTweetAdapter;
 import org.bittweet.android.ui.adapters.TweetAdapter;
 import org.bittweet.android.ui.adapters.TweetViewHolder;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
 import twitter4j.UserMentionEntity;
 
 public class NewTweetActivity extends Activity {
@@ -29,12 +41,53 @@ public class NewTweetActivity extends Activity {
 
     private EditText viewTweetEdit;
     private TextView viewCharCounter;
+    private Button attachImage;
+    private ImageView uploadImage;
+
+    private final int SELECT_PHOTO = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_tweet);
         initializeResources();
+
+        uploadImage = (ImageView) findViewById(R.id.image);
+        attachImage = (Button) findViewById(R.id.attachImage);
+        attachImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent photoPickerIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(photoPickerIntent, SELECT_PHOTO);
+            }
+        });
+    }
+
+    /* Photo Selection result */
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == SELECT_PHOTO && resultCode == Activity.RESULT_OK) {
+            Activity activity = NewTweetActivity.this;
+            Bitmap bitmap = getBitmapFromCameraData(data, activity);
+            if (bitmap != null) {
+                uploadImage.setVisibility(View.VISIBLE);
+                uploadImage.setImageBitmap(bitmap);
+            }
+            else
+                System.err.println("Could not attach image");
+        }
+    }
+
+    private static Bitmap getBitmapFromCameraData(Intent data, Context context) {
+        try {
+            final Uri imageUri = data.getData();
+            final InputStream imageStream = context.getContentResolver().openInputStream(imageUri);
+            final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
+            return selectedImage;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     private void initializeResources() {
@@ -144,4 +197,5 @@ public class NewTweetActivity extends Activity {
 
         }
     }
+
 }
