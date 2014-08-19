acd929fbb3ef44da13e750b52b5d13cb12854ab8
fuyutsukikaru <hirokinoryu@gmail.com>
2014-08-19 15:10:49 -0700

    Improved blur to work with all images (only worked with Maki before)

--- bittweet/src/main/java/org/bittweet/android/ui/Blur.java
+++ bittweet/src/main/java/org/bittweet/android/ui/Blur.java
@@ -3,6 +3,7 @@ package org.bittweet.android.ui;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.RectF;
 import android.support.v8.renderscript.Allocation;
 import android.support.v8.renderscript.Element;
 import android.support.v8.renderscript.RenderScript;
@@ -29,6 +30,40 @@ public class Blur {
         v.layout(0, 0, v.getMeasuredWidth(), v.getMeasuredHeight());
         v.draw(c);
         return b;
+    }
+
+    // Center crop a bitmap without changing aspect ratio
+    public static Bitmap scaleCenterCrop(Bitmap source, int newWidth, int newHeight) {
+        int sourceWidth = source.getWidth();
+        int sourceHeight = source.getHeight();
+
+        // Compute the scaling factors to fit the new height and width, respectively.
+        // To cover the final image, the final scaling will be the bigger
+        // of these two.
+        float xScale = (float) newWidth / sourceWidth;
+        float yScale = (float) newHeight / sourceHeight;
+        float scale = Math.max(xScale, yScale);
+
+        // Now get the size of the source bitmap when scaled
+        float scaledWidth = scale * sourceWidth;
+        float scaledHeight = scale * sourceHeight;
+
+        // Let's find out the upper left coordinates if the scaled bitmap
+        // should be centered in the new size give by the parameters
+        float left = (newWidth - scaledWidth) / 2;
+        float top = (newHeight - scaledHeight) / 2;
+
+        // The target rectangle for the new, scaled version of the source bitmap will now
+        // be
+        RectF targetRect = new RectF(left, top, left + scaledWidth, top + scaledHeight);
+
+        // Finally, we create a new bitmap of the specified size and draw our new,
+        // scaled bitmap onto it.
+        Bitmap dest = Bitmap.createBitmap(newWidth, newHeight, source.getConfig());
+        Canvas canvas = new Canvas(dest);
+        canvas.drawBitmap(source, null, targetRect, null);
+
+        return dest;
     }
 
     // Blur bitmap and set to an imageview
@@ -73,7 +108,7 @@ public class Blur {
         image.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
             @Override
             public void onGlobalLayout() {
-                Bitmap scaled = Bitmap.createScaledBitmap(copy, orig.getWidth(), orig.getHeight(), false);
+                Bitmap scaled = scaleCenterCrop(copy, orig.getWidth(), orig.getHeight());
                 Bitmap cropped = Bitmap.createBitmap(scaled, 0, orig.getHeight() - image.getHeight(), orig.getWidth(), image.getHeight());
 
                 image.setImageBitmap(cropped);
