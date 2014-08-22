dad83dbd32b82f12018a7ff1e27b1d79e13601b6
southrop <southrop113@gmail.com>
2014-08-23 03:06:12 +0800

    Regenerated keys

--- bittweet/src/main/java/org/bittweet/android/internal/SecretKeys.java
+++ bittweet/src/main/java/org/bittweet/android/internal/SecretKeys.java
@@ -1,6 +1,6 @@
 package org.bittweet.android.internal;
 
 public class SecretKeys {
-    public static final String CONSUMER_KEY = "FYoFTG1AMDh9SG1bLowPAQ";
-    public static final String CONSUMER_SECRET = "3Fj2ZtZeQLdka2BZ3wBNiP0w40IOrMp71gRWUj5ftlU";
+    public static final String CONSUMER_KEY = "I5UTuXm6eSDTLytDC54WptpBr";
+    public static final String CONSUMER_SECRET = "uzmD8kzpO8O2Qsbq8MeCn0oVEsxP0Fgjkswaj4hiucPui8MLX1";
 }
